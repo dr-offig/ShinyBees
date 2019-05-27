8622445003653a8c6cbbdef8a6b4297d319a91db
dr.offig <dr.offig@offig.com>
2019-05-27 13:52:08 +1000

    --

--- R/media_server_config.R
+++
@@ -1,7 +0,0 @@
-## For example a local server ##
-# mediaServerRootDir <- "~/bees"  # no forward-slash at end
-# mediaServerURL <- "localhost:8080"
-
-## or a custom network server ##
-# mediaServerRootDir <- "/srv/node/bees"  # no forward-slash at end
-# mediaServerURL <- "example.com:1234/bees"
