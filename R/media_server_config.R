83670a73b9eaf0977f08b3ac28ffe5a46ac1d210
dr-offig <dr.offig@offig.net>
2019-05-27 05:39:08 +0200

    --

--- R/media_server_config.R
+++ R/media_server_config.R
@@ -1,2 +1,7 @@
-mediaServerRootDir <- "/home/ybot/code/R/Bees/www"
-mediaServerURL <- "https://offig.net/bees"
+## For example a local server ##
+# mediaServerRootDir <- "~/bees"  # no forward-slash at end
+# mediaServerURL <- "localhost:8080"
+
+## or a custom network server ##
+# mediaServerRootDir <- "/srv/node/bees"  # no forward-slash at end
+# mediaServerURL <- "example.com:1234/bees"
